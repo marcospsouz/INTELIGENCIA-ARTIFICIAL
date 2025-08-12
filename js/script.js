708b7bdad27336008a534cbe2c5c3704186a6fb6
marcos/psouza <137806232+marcospsouz@users.noreply.github.com>
2025-08-12 11:24:29 -0300

    fds

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
